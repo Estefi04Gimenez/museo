615cdb62cb6044e5603db7aa95e7290706a0f558
SimonRossini <simonrossin@gmail.com>
2022-09-15 16:30:25 -0300

    correcciones de nomenclatura

--- iPortfolio/index.php
+++ iPortfolio/index.php
@@ -1,11 +1,11 @@
 <?php
 require "conexion.php";
 #al acceder a la pagina una de estas variables deberia completarse con el id del museo correspondiente
-#$id = $GET_['id'];
+$id = $GET_['id'];
 #$id = $POST_['id'];
 
 #por el momento trabajamos con esta variable. hasta crear una pagina para acceder a distintos museos
-$id = 1;
+#$id = 1;
 
 $q = "SELECT nombre FROM museos WHERE id_museo = '$id'";
 
